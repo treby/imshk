f034b10e17fe3b6dba1e0f4199f5c5580d9e87fc
Hiroaki Ninomiya <me@treby.info>
2016-12-17 15:52:49 +0900

    refactoring(classify)

--- konomin-bot.rb
+++ konomin-bot.rb
@@ -1,22 +1,54 @@
 require 'octokit'
 require 'git'
 
-repo = 'git@github.com:treby/mlborder.git'
-target_dir = 'mlborder'
-git_name = 'konomin-bot'
-git_email = 'mlborder@atelier-nodoka.net'
+class KonominBotExecutor
+  TARGET_REPOSITORY = 'treby/mlborder'
+  TARGET_DIR = 'mlborder'
+  GIT_USER_NAME = 'konomin-bot'
+  GIT_USER_EMAIL = 'mlborder@atelier-nodoka.net'
 
-g = Git.clone(repo, '', path: target_dir)
-g.config('user.name', git_name)
-g.config('user.email', git_email)
-branch_name = "update-seed-#{Time.now.strftime('%F-%H-%M')}"
-g.branch(branch_name).checkout
+  def execute
+    git_agent.tap do |g|
+      g.config('user.name', GIT_USER_NAME)
+      g.config('user.email', GIT_USER_EMAIL)
+      g.branch(branch_name).checkout
+    end
 
-`heroku run --app mlborder rails runner 'ActiveRecord::Base.logger = nil; puts Event.dump_seeds' | nkf -Lu > #{target_dir}/db/seeds.rb`
-g.add
-g.commit('Update db/seeds.rb')
-g.push('origin', branch_name)
+    `heroku run --app mlborder rails runner 'ActiveRecord::Base.logger = nil; puts Event.dump_seeds' | nkf -Lu > #{TARGET_DIR}/db/seeds.rb`
 
-cli = Octokit::Client.new(login: ENV['KONOMIN_USERNAME'], password: ENV['KONOMIN_PASSWORD'])
-cli.login
-cli.create_pull_request 'treby/mlborder', 'master', branch_name, 'お疲れ様、プロデューサー', 'seedファイルを更新しておいたわよ'
+    git_agent.tap do |g|
+      g.add
+      g.commit("Update db/seeds.rb #{time_stamp}")
+      g.push('origin', branch_name)
+    end
+
+    github_agent.create_pull_request TARGET_REPOSITORY, 'master', branch_name, *message_from_konomin
+  end
+
+  def message_from_konomin
+    ['お疲れ様、プロデューサー',
+     'seedファイルを更新しておいたわよ']
+  end
+
+  def git_agent
+    @git_agent ||= Git.clone("git@github.com:#{TARGET_REPOSITORY}.git", '', path: TARGET_DIR)
+  end
+
+  def github_agent
+    unless @cli
+      @cli = Octokit::Client.new(login: ENV['KONOMIN_USERNAME'], password: ENV['KONOMIN_PASSWORD'])
+      @cli.login
+    end
+    @cli
+  end
+
+  def branch_name
+    @branch_name ||= "update-seed-#{time_stamp}"
+  end
+
+  def time_stamp
+    @time_stamp ||= Time.now.strftime('%Y%m%d-%H%M')
+  end
+end
+
+KonominBotExecutor.new.execute
