1f55ac7ffbf19669a2e8192a33991344cb302cf2
Hiroaki Ninomiya <me@treby.info>
2016-12-17 14:46:31 +0900

    create new branch for push

--- konomin-bot.rb
+++ konomin-bot.rb
@@ -9,11 +9,13 @@ git_email = 'mlborder@atelier-nodoka.net'
 g = Git.clone(repo, '', path: target_dir)
 g.config('user.name', git_name)
 g.config('user.email', git_email)
+branch_name = 'by-konomin-assistant'
+g.branch(branch_name).checkout
 
 `heroku run --app mlborder rails runner 'ActiveRecord::Base.logger = nil; puts Event.dump_seeds' | nkf -Lu > #{target_dir}/db/seeds.rb`
 g.add
-g.commit('Hello, world!!')
+g.commit('Update db/seeds.rb')
+g.push
 
 #cli = Octokit::Client.new(login: ENV['KONOMIN_USERNAME'], password: ENV['KONOMIN_PASSWORD'])
 #p Octokit.user
-
